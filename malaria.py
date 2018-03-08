ec27afa7c1befd25d565363bcac66cd45f574d6a
Myrthe Moring <mmoring@itsavirus.com>
2018-03-08 15:43:47 +0100

    malaria

--- malaria.py
+++ malaria.py
@@ -17,7 +17,8 @@ Simple rule which achieves realistic complex features:
 
 import numpy as np
 import math
-from pyics import Model
+import random
+import itertools
 
 def decimal_to_base_k(n, k):
     """Converts a given decimal (i.e. base-10 integer) to a list containing the
@@ -38,24 +39,96 @@ def base_to_decimal(base, k):
     return n
 
 
-class Cell:
-    def __init__(self, chance):
-        self.prev_state = 0
-        self.state = 0
-        self.lived = 0
-        if random.random() <= chance:
-            self.prevState = 1
-            self.state = 1
-            self.lived += 1
+class Grid:
+    def __init__(self, mosq_count, width=100, height=100, population=0.5):
+        self.population = population
+        self.mosq_count = mosq_count
+        self.humans = []
+        self.mosquitoes = []
+        self.width = width
+        self.height = height
+
+        self.create_humans()
+        self.create_mosquitoes(self.mosq_count)
+
+    def create_humans(self):
+        number_humans = int(self.width*self.height*self.population)
+        positions_all = list(itertools.product(range(self.width), range(self.height)))
+        positions = random.sample(positions_all, number_humans)
+        for i in range(number_humans):
+            self.humans.append(Humans(positions[i]))
+
+    def create_mosquitoes(self, mosq_count):
+        for i in range(mosq_count):
+            x_pos = random.randint(0, self.width-1)
+            y_pos = random.randint(0, self.height-1)
+            pos = (x_pos, y_pos)
+            self.mosquitoes.append(Mosquitoes(pos))
+
+    def step(self):
+        """ Steps per day """
+        self.birth_mosquitoes()
+
+        for mosq in self.mosquitoes:
+            if mosq.mosquitoe_check() == False:
+                (self.mosquitoes).remove(mosq)
+                del(mosq)
+            mosq.step_mosquitoe(self.width, self.height)
+
+    def birth_mosquitoes(self):
+        """ Create every day (step) new mosquitoes based on the number of mosquitoes.
+        TODO: infected/not infected mosquitoes"""
+        new_mosq = self.mosq_count * 8
+        self.create_mosquitoes(new_mosq)
 
 class Humans:
-    def __init__(self):
-        self.width = 100
-        self.heigth = 100
+    def __init__(self, position):
+        """
+        States:
+        - 0: susceptible
+        - 1: infected
+        - 2: immune
+        - 3: dead
+        """
+        self.state = 0
+        self.position = (0,0)
+
+class Mosquitoes:
+    def __init__(self, position):
+        self.position = position
+        self.infected = False
+        # ophogen van hungry tot 3 (dan dood)
+        self.hungry = 0
+        self.human = None
+        self.age = 0
+
+    def random_walk_Moore(self, width, height):
+        change_pos = (random.randint(-1,1), random.randint(-1,1))
+        while change_pos == (0,0):
+            change_pos = (random.randint(-1,1), random.randint(-1,1))
+
+        new_x = self.position[0] + change_pos[0]
+        while (new_x < 0 or new_x > width):
+            new_x = self.position[0] + random.randint(-1,1)
+
+        new_y = self.position[1] + change_pos[1]
+        while (new_y < 0 or new_y > height):
+            new_y = self.position[1] + random.randint(-1,1)
+            print(self.position[1])
+
+        new_pos = (self.position[0] + new_x,self.position[1] + new_y)
+        self.position = new_pos
+
+    def mosquitoe_check(self):
+        if self.hungry > 3 or self.age > 5:
+            return False
+
+    def step_mosquitoe(self, width, height):
+        self.age += 1
+        self.random_walk_Moore(width, height)
 
 
 if __name__ == '__main__':
-    from pyics import GUI
     import collections
     import matplotlib
     import matplotlib.pyplot as plt
@@ -63,3 +136,8 @@ if __name__ == '__main__':
     import random
     from random import randint
     from collections import Counter
+
+    malaria_grid = Grid(20)
+
+    malaria_grid.step()
+    # malaria_grid.step()
